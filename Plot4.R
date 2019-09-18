94c7bb9d7d5ec4cd027d0f63bc3fa6d822ce8dbb
iamnikhithabhat <47333294+iamnikhithabhat@users.noreply.github.com>
2019-09-18 11:46:13 +0530

    Add files via upload

---
+++ Plot4.R
@@ -0,0 +1,17 @@
+library("data.table")
+
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+
+# Prevents Scientific Notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Making a POSIXct date capable of being filtered and graphed by time of day
+# Plot 4
+plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
